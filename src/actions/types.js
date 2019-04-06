081a7164a772b391fcc5f3d379cf750356b850fd
Louis Shi <Shilou2010@gmail.com>
2019-04-06 15:35:52 -0400

    employeecreateform

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,5 @@ export const PASSWORD_CHANGED = 'password_changed';
 export const LOGIN_USER_SUCCESS = 'login_user_success';
 export const LOGIN_USER_FAIL = 'LOGIN_USER_FAIL';
 export const LOGIN_USER = 'login_user';
+
+export const EMPLOYEE_UPDATE = 'employee_update';
